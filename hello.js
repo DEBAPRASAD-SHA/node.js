9ab673f02b9751c18a5d3ecb424800bacec929f2
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-08-21 15:31:52 +0530

    math function

--- hello.js
+++ hello.js
@@ -1,1 +1,6 @@
-console.log("Hey there! I am JS");
+// console.log("Hey there! I am JS");
+
+const math = require("./math")
+
+
+console.log("math value is ", math);
---
+++ math.js
@@ -0,0 +1,11 @@
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+module.exports = "1";
+module.exports = "2";
+
