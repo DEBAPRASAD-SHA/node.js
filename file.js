0d2102021cfcbe099afb6f1354600e9afac168c1
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-08-22 11:25:12 +0530

    file handling

--- file.js
+++ file.js
@@ -10,10 +10,13 @@ const fs = require("fs");
 // const result = fs.readFileSync("./contact.txt", "utf-8");
 // console.log(result);
 
-fs.readFile("./contact.txt", "utf-8", (err, result) => {
-    if (err) {
-        console.log("Error", err)
-    } else {
-        console.log(result);
-    }
-})
+// fs.readFile("./contact.txt", "utf-8", (err, result) => {
+//     if (err) {
+//         console.log("Error", err)
+//     } else {
+//         console.log(result);
+//     }
+// });
+
+
+fs.appendFileSync("./test.txt", new Date().getDate().toString());
