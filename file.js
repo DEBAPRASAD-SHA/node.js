3990346b01fe36237114984f61c6b6ce34cdb4e8
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-08-22 10:55:14 +0530

    file handling

--- file.js
+++ file.js
@@ -7,5 +7,13 @@ const fs = require("fs");
 // Asynchronous...
 // fs.writeFile('./test.txt', 'i am Rock Async', (err) => {});
 
-const result = fs.readFileSync("./contact.txt", "utf-8");
-console.log(result);
+// const result = fs.readFileSync("./contact.txt", "utf-8");
+// console.log(result);
+
+fs.readFile("./contact.txt", "utf-8", (err, result) => {
+    if (err) {
+        console.log("Error", err)
+    } else {
+        console.log(result);
+    }
+})
